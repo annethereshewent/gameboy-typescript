c8e4506f1f4d93eecccb678d318f5baa649327ac
Anne Castrillon <anne.castrillon@gmail.com>
2023-04-26 19:02:28 -0700

    add more instructions

--- src/gameboy/cpu/setInstructionMap.ts
+++ src/gameboy/cpu/setInstructionMap.ts
@@ -636,4 +636,948 @@ export function setInstructionMap(this: CPU) {
 
     }
   })
+
+  this.instructionMap.set(0x50, {
+    name: "LD D, B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x51, {
+    name: "LD D, C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x52, {
+    name: "LD D, D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x53, {
+    name: "LD D, E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x54, {
+    name: "LD D, H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x55, {
+    name: "LD D, L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x56, {
+    name: "LD D, (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x57, {
+    name: "LD D, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x58, {
+    name: "LD E, B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x59, {
+    name: "LD E, C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x5A, {
+    name: "LD E, D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x5B, {
+    name: "E, E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x5C, {
+    name: "LD E, H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x5D, {
+    name: "LD E, L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x5E, {
+    name: "LD E, (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x5F, {
+    name: "LD E, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x60, {
+    name: "LD H, B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x61, {
+    name: "LD H, C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x62, {
+    name: "LD H, D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x63, {
+    name: "LD H, E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x64, {
+    name: "LD H, H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x65, {
+    name: "LD H, L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x66, {
+    name: "H, (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x67, {
+    name: "LD H, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x68, {
+    name: "LD L, B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x69, {
+    name: "LD L, C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x6A, {
+    name: "LD L, D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x6B, {
+    name: "LD L, E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x6C, {
+    name: "LD L, H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x6D, {
+    name: "LD L, L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x6E, {
+    name: "LD L, (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x6F, {
+    name: "LD L, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x70, {
+    name: "LD (HL), B",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x71, {
+    name: "LD (HL) C",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x72, {
+    name: "LD (HL), D",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x73, {
+    name: "LD (HL), E",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x74, {
+    name: "LD (HL), H",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x75, {
+    name: "LD (HL), L",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x76, {
+    name: "HALT",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x77, {
+    name: "LD (HL), A",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x78, {
+    name: "LD A, B",
+    cycleTime: ,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x79, {
+    name: "LD A, C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x7A, {
+    name: "LD A, D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x7B, {
+    name: "LD A, E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x7C, {
+    name: "LD A, H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x7D, {
+    name: "LD A, L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x7E, {
+    name: "LD A, (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x7F, {
+    name: "LD A, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x80, {
+    name: "ADD A, B",
+    cycleTime: 4,
+    operation: () => {
+        this.registers.add(this.registers.A, this.registers.B)
+    }
+  })
+
+  this.instructionMap.set(0x81, {
+    name: "ADD A, C",
+    cycleTime: 4,
+    operation: () => {
+      this.registers.add(this.registers.A, this.registers.B)
+    }
+  })
+
+  this.instructionMap.set(0x82, {
+    name: "ADD A, D",
+    cycleTime: 4,
+    operation: () => {
+      this.registers.add(this.registers.A, this.registers.D)
+    }
+  })
+
+  this.instructionMap.set(0x83, {
+    name: "ADD A, E",
+    cycleTime: 4,
+    operation: () => {
+      this.registers.add(this.registers.A, this.registers.D)
+    }
+  })
+
+  this.instructionMap.set(0x84, {
+    name: "ADD A, H",
+    cycleTime: 4,
+    operation: () => {
+      this.registers.add(this.registers.A, this.registers.H)
+    }
+  })
+
+  this.instructionMap.set(0x85, {
+    name: "ADD A, L",
+    cycleTime: 4,
+    operation: () => {
+      this.registers.add(this.registers.A, this.registers.L)
+    }
+  })
+
+  this.instructionMap.set(0x86, {
+    name: "ADD A, (HL)",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x87, {
+    name: "ADD A, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x88, {
+    name: "ADC A, B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x89, {
+    name: "ADC A, C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x8A, {
+    name: "ADC A, D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x8B, {
+    name: "ADC A, E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x8C, {
+    name: "ADC A, H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x8D, {
+    name: "ADC A, L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x8E, {
+    name: "ADC A, (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x8F, {
+    name: "ADC A, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x90, {
+    name: "SUB B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x91, {
+    name: "SUB C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x92, {
+    name: "SUB D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x93, {
+    name: "SUB E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x94, {
+    name: "SUB H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x95, {
+    name: "SUB L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x96, {
+    name: "SUB (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x97, {
+    name: "SUB A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x98, {
+    name: "SBC A, B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x99, {
+    name: "SBC A, C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x9A, {
+    name: "SBC A, D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x9B, {
+    name: "SBC A, E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x9C, {
+    name: "SBC A, H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x9D, {
+    name: "SBC A, L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x9E, {
+    name: "SBC A, (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0x9F, {
+    name: "SBC A, A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA0, {
+    name: "AND B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA1, {
+    name: "AND C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA2, {
+    name: "AND D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA3, {
+    name: "AND E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA4, {
+    name: "AND H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA5, {
+    name: "AND L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA6, {
+    name: "AND (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA7, {
+    name: "AND A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA8, {
+    name: "XOR B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xA9, {
+    name: "XOR C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xAA, {
+    name: "XOR D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xAB, {
+    name: "XOR E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xAC, {
+    name: "XOR H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xAD, {
+    name: "XOR L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xAE, {
+    name: "XOR (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xAF, {
+    name: "XOR A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB0, {
+    name: "OR B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB1, {
+    name: "OR C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB2, {
+    name: "OR D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB3, {
+    name: "OR E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB4, {
+    name: "OR H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB5, {
+    name: "OR L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB6, {
+    name: "OR (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB7, {
+    name: "OR A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB8, {
+    name: "CP B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB9, {
+    name: "CP C",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBA, {
+    name: "CP D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB8, {
+    name: "CP E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB9, {
+    name: "CP H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBA, {
+    name: "CP (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBB, {
+    name: "CP B",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xB9, {
+    name: "CP C",
+    cycleTime: ,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBA, {
+    name: "CP D",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBB, {
+    name: "CP E",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBC, {
+    name: "CP H",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBD, {
+    name: "CP L",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBE, {
+    name: "CP (HL)",
+    cycleTime: 8,
+    operation: () => {
+
+    }
+  })
+
+  this.instructionMap.set(0xBF, {
+    name: "CP A",
+    cycleTime: 4,
+    operation: () => {
+
+    }
+  })
 }
