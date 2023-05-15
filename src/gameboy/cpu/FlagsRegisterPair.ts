69e623257d653b2111e8a9d718f7f5de52858463
Anne Castrillon <anne.castrillon@gmail.com>
2023-05-15 11:57:49 -0700

    minor changes

--- src/gameboy/cpu/FlagsRegisterPair.ts
+++ src/gameboy/cpu/FlagsRegisterPair.ts
@@ -2,10 +2,10 @@ import { CPURegister } from "./CPURegister"
 
 export class FlagsRegisterPair extends CPURegister {
   set value(newVal: number) {
-    // clear the first four bits after setting the new value
-    const clearFlags = 0b1111111111110000
+    // clear the first four bits after setting the new value, they're never used in F
+    const clearBits = 0b1111111111110000
 
-    this.dataView.setUint16(this.registerId, newVal & clearFlags, true)
+    this.dataView.setUint16(this.registerId, newVal & clearBits, true)
   }
 
   get value() {
